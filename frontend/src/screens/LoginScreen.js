a6c23d4b95f65469a5a8bb07fa363dbaacd0798b
Yashashvi Maurya <yashashvimaurya@gmail.com>
2021-06-29 01:53:54 +0530

    login screen completed

--- frontend/src/screens/LoginScreen.js
+++ frontend/src/screens/LoginScreen.js
@@ -8,7 +8,8 @@ import {login} from '../actions/userActions'
 
 import FormContainer from '../components/FormContainer'
 
-const LoginScreen = ({location}) => {
+
+const LoginScreen = ({location , history }) => {
     const [email , setEmail] = useState('')
     const [password , setPassword] = useState('')
 
@@ -16,14 +17,25 @@ const LoginScreen = ({location}) => {
     const userLogin = useSelector(state => state.userLogin)
     const { loading , error , userInfo  } = userLogin
      
+
+    const [subs , setSubs] = useState()
     const redirect = location.search ? location.search.split('=')[1] : '/'
+
+    useEffect(()=>{
+       if(userInfo){
+          history.push(redirect)
+       }
+    } , [history , redirect , userInfo ] )
 
     const submitHandler = (e)=>{
         e.preventDefault()
+        dispatch(login(email , password))
     }
 
     return <FormContainer>
         <h1>Sign In</h1>
+        {error && <Message variant='danger' >{error}</Message>}
+
         <Form onSubmit={submitHandler} >
            <Form.Group controlId='email' >
               <Form.Label>Email Address</Form.Label>
@@ -36,13 +48,13 @@ const LoginScreen = ({location}) => {
            <Form.Group controlId='password' >
               <Form.Label>Password</Form.Label>
               <Form.Control type='passwprd' placeholder='Enter Password' value ={password} 
-                 onChange={(e)=> setEmail(e.target.value) }
+                 onChange={(e)=> setPassword(e.target.value) }
                >
 
               </Form.Control>
            </Form.Group> 
            
-           <Button type='submit' variant='primary' > 
+           <Button  type='submit' variant='primary' > 
               Sign In
            </Button>
 
